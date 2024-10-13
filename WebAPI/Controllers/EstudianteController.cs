eaaa6506040a2dfd3f183e07b43c910951d47fce
KarlaBC <karlatataianabc@gmail.com>
2024-10-13 07:43:16 -0600

    Se crean los metodos Get por id, Put y Delete.

--- WebAPI/Controllers/EstudianteController.cs
+++ WebAPI/Controllers/EstudianteController.cs
@@ -24,5 +24,76 @@ namespace WebAPI.Controllers
         {
             return await _context.Estudiantes.ToListAsync();
         }
+
+        [HttpGet]
+        [Route("obtener/{id}")]
+        public async Task<ActionResult<EstudianteModel>> GetEstudianteById(int id)
+        {
+            var estudiante = await _context.Estudiantes.FindAsync(id);
+
+            if (estudiante == null)
+            {
+                return NotFound();
+            }
+
+            return estudiante;
+        }
+
+        [HttpPost]
+        [Route("crear")]
+        public async Task<ActionResult<EstudianteModel>> PostEstudiante([FromBody] EstudianteModel estudiante)
+        {
+            _context.Estudiantes.Add(estudiante);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetEstudianteById), new { id = estudiante.id }, estudiante);
+        }
+
+        [HttpPut]
+        [Route("actualizar/{id}")]
+        public async Task<IActionResult> PutEstudiante(int id, [FromBody] EstudianteModel estudiante)
+        {
+            if (id != estudiante.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(estudiante).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Estudiantes.Any(e => e.id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+        public async Task<IActionResult> DeleteEstudiante(int id)
+        {
+            var estudiante = await _context.Estudiantes.FindAsync(id);
+            if (estudiante == null)
+            {
+                return NotFound();
+            }
+
+            _context.Estudiantes.Remove(estudiante);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
     }
 }
